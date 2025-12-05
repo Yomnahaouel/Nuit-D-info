f0ba74e511fb94639d275b6b295c6c4259369675
yomna <haouelyomna111@gmail.com>
2025-12-05 07:26:16 +0100

    commit 3

--- NuitDInfo/NuitDInfo/settings.py
+++ NuitDInfo/NuitDInfo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e7_wut8a7ro85g=l^jo$7_9e52yvhnb*q^)4m!o(+y%x@tea=x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
